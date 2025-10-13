a9a16096fc99b853a13e7843226fc395435daebe
inquerafael <56138748+inquerafael@users.noreply.github.com>
2025-10-13 19:16:27 -0300

    classe base e view para treino

---
+++ prg03InqueRafael/src/main/java/br/com/ifba/atividade06/classeEObjetos/ContaBanco.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade06.classeEObjetos;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ContaBanco {
+    
+    private int numConta;
+    protected String tipo;
+    private String dono;
+    private int saldo;
+    private boolean status = false; 
+
+    //construtor
+    public ContaBanco(int numConta, String tipo, String dono, int saldo, boolean status) {
+        this.numConta = numConta;
+        this.tipo = tipo;
+        this.dono = dono;
+        this.saldo = saldo;
+        this.status = true;
+    }
+
+    //gets e sets
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+    
+    
+    
+}
