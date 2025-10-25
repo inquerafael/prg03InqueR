1fcc21e1f0b4242b02aeb0a6264b16e175a9c78c
inquerafael <56138748+inquerafael@users.noreply.github.com>
2025-10-25 12:32:51 -0300

    create interface

---
+++ prg03InqueRafael/src/main/java/br/com/ifba/atividade09/encapsulamento/Pagamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.atividade09.encapsulamento;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface Pagamento {
+    
+    public double calcularTotal();
+    public void imprimirRecibo();
+    
+}
